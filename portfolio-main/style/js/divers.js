eecebdc614dc4e4bf1a1142707f7ba8844f3fc4f
Florianguetat <159165523+Florianguetat@users.noreply.github.com>
2026-01-29 08:29:50 +0100

    Delete portfolio-main directory

--- portfolio-main/style/js/divers.js
+++
@@ -1,6 +0,0 @@
-// Gestion des cartes cliquables
-document.querySelectorAll('.toggle-btn').forEach(btn => {
-  btn.addEventListener('click', () => {
-    btn.parentElement.classList.toggle('open');
-  });
-});
