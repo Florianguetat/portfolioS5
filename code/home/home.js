e087b331ba21f60be2ee725f4bb93d641726403d
Florianguetat <159165523+Florianguetat@users.noreply.github.com>
2026-02-03 08:42:47 +0100

    Update language URLs for redirection

--- code/home/home.js
+++ code/home/home.js
@@ -1,12 +1,12 @@
 function redirectToLanguage(languageCode) {
-    const urls = {
-        '1': './français/accueil.html',
-        '2': './english/home.html',
-        '3': './italiano/iniziale.html',
-        '4': 'español/inicio.html',
-        '5': './deutsch/startseite.html',
-        '6': './português/inicial.html'
-    };
-    
-    window.location.href = urls[languageCode];
+  const urls = {
+    '1': '../francais/accueil.html',
+    '2': '../english/home.html',
+    '3': '../italiano/iniziale.html',
+    '4': '../espanol/inicio.html',
+    '5': '../deutsch/startseite.html',
+    '6': '../portugues/inicial.html'
+  };
+
+  window.location.href = urls[languageCode];
 }
